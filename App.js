1b06acb1482700ecfadb2dba41896b6bf78c8dc5
Atzel73 <79029695+Atzel73@users.noreply.github.com>
2024-03-22 22:50:05 -0600

    stack navigator added/ login finished

--- App.js
+++ App.js
@@ -3,18 +3,34 @@ import { StyleSheet, Text, View } from 'react-native';
 
 
 
+import 'react-native-gesture-handler';
+
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+
 import Login from './Screens/screenLogin/login';
+
+
+const Stack = createStackNavigator();
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      
-      <StatusBar style="auto" />
-    </View>
+
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Login} />
+      </Stack.Navigator>
+    </NavigationContainer>
+
   );
 }
 
 const styles = StyleSheet.create({
+  text: {
+    padding: 10,
+    bottom: -200,
+    textAlign: 'center',
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
--- Screens/screenLogin/login.js
+++ Screens/screenLogin/login.js
@@ -5,43 +5,43 @@ import TextInput from '../../Components/TextInput/textInput';
 import Button from '../../Components/Button/button';
 
 export default function Login() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center'
-      }}
-    >
-      <Text style={{ color: '#223e4b', fontSize: 20, marginBottom: 16 }}>
-        Login
-      </Text>
-      <View style={{ paddingHorizontal: 32, marginBottom: 16, width: '100%' }}>
-        <TextInput
-          icon="mail"
-          placeholder="Enter your email"
-          autoCapitalize="none"
-          autoCompleteType="email"
-          keyboardType="email-address"
-          keyboardAppearance="dark"
-          returnKeyType="next"
-          returnKeyLabel="next"
-        />
+    return (
+      <View
+        style={{
+          flex: 1,
+          backgroundColor: '#fff',
+          alignItems: 'center',
+          justifyContent: 'center'
+        }}
+      >
+        <Text style={{ color: '#223e4b', fontSize: 20, marginBottom: 16 }}>
+          Login
+        </Text>
+        <View style={{ paddingHorizontal: 32, marginBottom: 16, width: '100%' }}>
+          <TextInput
+            icon="mail"
+            placeholder="Enter your email"
+            autoCapitalize="none"
+            autoCompleteType="email"
+            keyboardType="email-address"
+            keyboardAppearance="dark"
+            returnKeyType="next"
+            returnKeyLabel="next"
+          />
+        </View>
+        <View style={{ paddingHorizontal: 32, marginBottom: 16, width: '100%' }}>
+          <TextInput
+            icon="key"
+            placeholder="Enter your password"
+            secureTextEntry
+            autoCompleteType="password"
+            autoCapitalize="none"
+            keyboardAppearance="dark"
+            returnKeyType="go"
+            returnKeyLabel="go"
+          />
+        </View>
+        <Button label="Login" onPress={() => true} />
       </View>
-      <View style={{ paddingHorizontal: 32, marginBottom: 16, width: '100%' }}>
-        <TextInput
-          icon="key"
-          placeholder="Enter your password"
-          secureTextEntry
-          autoCompleteType="password"
-          autoCapitalize="none"
-          keyboardAppearance="dark"
-          returnKeyType="go"
-          returnKeyLabel="go"
-        />
-      </View>
-      <Button label="Login" onPress={() => true} />
-    </View>
-  );
-}
+    );
+  }
