017f3187d102733bb8995e5780480f435da5fad5
Atzel73 <79029695+Atzel73@users.noreply.github.com>
2024-03-22 23:04:35 -0600

    config metro/ firestore added

---
+++ firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAzGrAjODUaV0xUR7tlOvbVmRyFSgNSXAM",
+  authDomain: "fireapp-47786.firebaseapp.com",
+  projectId: "fireapp-47786",
+  storageBucket: "fireapp-47786.appspot.com",
+  messagingSenderId: "429580976424",
+  appId: "1:429580976424:web:38d569ae94610ccdd9101d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
