f4989f9a7a4fdc9fd1ed17adc78e7b21e58fbf02
Atzel73 <79029695+Atzel73@users.noreply.github.com>
2024-03-23 21:13:18 -0600

    form redo, only is left formik!

--- Screens/screenLogin/login.js
+++ Screens/screenLogin/login.js
@@ -4,58 +4,66 @@ import { Text, View } from 'react-native';
 import TextInput from '../../Components/TextInput/textInput';
 import Button from '../../Components/Button/button';
 
-import { useFormik } from 'formik';
+import { Formik } from 'formik';
 
 
-
-const { handleChange, handleSubmit, values } = useFormik({
-  initialValues: { email: '', password: '' },
-  onSubmit: values =>
-    alert(`Email: ${values.email}, Password: ${values.password}`)
-});
 
 export default function Login() {
-  
-    return (
-      <View
-        style={{
-          flex: 1,
-          backgroundColor: '#fff',
-          alignItems: 'center',
-          justifyContent: 'center'
-        }}
-      >
-        <Text style={{ color: '#223e4b', fontSize: 20, marginBottom: 16 }}>
-          Login
-        </Text>
-        <View style={{ paddingHorizontal: 32, marginBottom: 16, width: '100%' }}>
-          <TextInput
-            icon="mail"
-            placeholder="Enter your email"
-            autoCapitalize="none"
-            autoCompleteType="email"
-            keyboardType="email-address"
-            keyboardAppearance="dark"
-            returnKeyType="next"
-            returnKeyLabel="next"
-            onChangeText={handleChange('email')}
-          />
-        </View>
-        <View style={{ paddingHorizontal: 32, marginBottom: 16, width: '100%' }}>
-          <TextInput
-            icon="key"
-            placeholder="Enter your password"
-            secureTextEntry
-            autoCompleteType="password"
-            autoCapitalize="none"
-            keyboardAppearance="dark"
-            returnKeyType="go"
-            returnKeyLabel="go"
-            onChangeText={handleChange('password')}
+  return (
+    <Formik
+      initialValues={{ email: "", password: "" }}
+      onSubmit={(values) => console.log(values.password, values.email)}
 
-          />
+    >
+      {({ handleChange, handleBlur, handleSubmit, values }) => (
+        <View
+          style={{
+            flex: 1,
+            backgroundColor: "#fff",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <Text style={{ color: "#223e4b", fontSize: 20, marginBottom: 16 }}>
+            Login
+          </Text>
+          <View
+            style={{ paddingHorizontal: 32, marginBottom: 16, width: "100%" }}
+          >
+            <TextInput
+              icon="mail"
+              placeholder="Enter your email"
+              autoCapitalize="none"
+              autoCompleteType="email"
+              keyboardType="email-address"
+              keyboardAppearance="dark"
+              returnKeyType="next"
+              returnKeyLabel="next"
+              onChangeText={handleChange("email")}
+              onBlur={handleBlur("email")}
+              value={values.email}
+            />
+          </View>
+          <View
+            style={{ paddingHorizontal: 32, marginBottom: 16, width: "100%" }}
+          >
+            <TextInput
+              icon="key"
+              placeholder="Enter your password"
+              secureTextEntry
+              autoCompleteType="password"
+              autoCapitalize="none"
+              keyboardAppearance="dark"
+              returnKeyType="go"
+              returnKeyLabel="go"
+              onChangeText={handleChange("password")}
+              onBlur={handleBlur("password")}
+              value={values.password}
+            />
+          </View>
+          <Button label="Login" onPress={handleSubmit} />
         </View>
-        <Button label="Login" onPress={handleSubmit} />
-      </View>
-    );
-  }
+      )}
+    </Formik>
+  );
+}
