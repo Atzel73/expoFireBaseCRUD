515570112e409c9b3d31a23c410491623686b933
Atzel73 <79029695+Atzel73@users.noreply.github.com>
2024-03-25 20:33:09 -0600

    final

--- App.js
+++ App.js
@@ -1,10 +1,5 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
-
-
 import 'react-native-gesture-handler';
-
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
 
--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,12 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-
-
 import { collection, addDoc } from "firebase/firestore"; 
-
-
-
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -19,11 +14,8 @@ const firebaseConfig = {
   messagingSenderId: "429580976424",
   appId: "1:429580976424:web:38d569ae94610ccdd9101d"
 };
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-
 // Initialize Cloud Firestore and get a reference to the service
 const db = getFirestore(app);
 
